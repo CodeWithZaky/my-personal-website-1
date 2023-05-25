4abf33f086f6dfb806becbd97fb8da6c0f6d0c33
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-05-25 08:13:50 +0700

    fix css

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
-import React from "react";
 import { Metadata } from "next";
+// import Image from "next/image";
+// import profilIMG from "@/assets/profil2.jpg";
 
 export const metadata: Metadata = {
   title: "About - Portfolio",
@@ -7,10 +8,10 @@ export const metadata: Metadata = {
 
 const About = () => {
   return (
-    <div className="w-[80%] mx-auto pb-10 px-10 mt-10 font-mono border flex justify-between items-start">
+    <div className="w-[80%] m-auto pb-10 px-10 font-mono border flex justify-between items-start">
       <div className="w-2/3 border space-y-10">
         <p className="text-5xl font-bold border">About Me</p>
-        <div className="list-disc">
+        <div className="space-y-4 pr-5">
           <p>
             My name is Ahmad Zaky Ubaidillah, but you can call me Zaky or Ubed.
           </p>
@@ -29,7 +30,13 @@ const About = () => {
           <p>Trying to be a little better than yesterday every day.</p>
         </div>
       </div>
-      <div className="w-1/3 border h-full" />
+      <div className="w-1/3 border h-full flex justify-end items-start">
+        {/* <Image
+          alt="image"
+          src={profilIMG}
+          className="bg-no-repeat object-cover"
+        /> */}
+      </div>
     </div>
   );
 };
