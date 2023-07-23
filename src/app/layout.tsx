8ee2570db62a5b07aa5061feb57b27235a333e13
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-07-23 20:35:04 +0700

    fixing main layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,26 +12,7 @@ export default function RootLayout({ children }: Props) {
       <body>
         <main className="container-none w-full min-h-screen bg-slate-100">
           <MainNavbar />
-          <section className="bg-black w-full relative">
-            <TriangleHome />
-            <div className="relative mx-auto w-[70%] flex flex-col border-x border-slate-300">
-              {children}
-              <div className="w-[15px] h-[15px] bg-red-400 absolute left-0 top-20" />
-              <div className="w-[1px] h-[100%] bg-slate-300 absolute left-[300px] top-0" />
-            </div>
-          </section>
-          <section className="LATEST-PROJECT w-full">
-            <div className="mx-auto w-[70%] flex flex-col border-l border-black py-10 gap-3">
-              <div className="text-7xl text-red-500 font-extrabold">
-                LATEST PROJECT
-              </div>
-              <div className="grid grid-cols-3 gap-4">
-                <LatestProject />
-                <LatestProject />
-                <LatestProject />
-              </div>
-            </div>
-          </section>
+          {children}
         </main>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import profilIMG from "@/assets/profil2.jpg";
 import { Metadata } from "next";
 import ButtonLearnMore from "../components/button-learn-more";
+import TriangleHome from "../components/triangle-home";
+import LatestProject from "../components/latest-project";
 
 export const metadata: Metadata = {
   title: "Home - Portfolio",
@@ -9,18 +11,40 @@ export const metadata: Metadata = {
 
 const Home = () => {
   return (
-    <div className="flex flex-col justify-center items-center m-auto gap-5 py-20">
-      <h3 className="text-2xl font-bold text-slate-100 text-center">
-        welcome to my portfolio
-      </h3>
-      <h1 className="text-8xl font-bold text-slate-100 text-center">
-        {"I'M ZAKY"}
-      </h1>
-      <h1 className="text-5xl font-bold text-slate-100 text-center">
-        {"FRONTEND WEB DEVELOPER"}
-      </h1>
-      <ButtonLearnMore />
-    </div>
+    <>
+      {" "}
+      <section className="bg-black w-full relative">
+        <TriangleHome />
+        <div className="relative mx-auto w-[70%] flex flex-col border-x border-slate-300">
+          <div className="flex flex-col justify-center items-center m-auto gap-5 py-20">
+            <h3 className="text-2xl font-bold text-slate-100 text-center">
+              welcome to my portfolio
+            </h3>
+            <h1 className="text-8xl font-bold text-slate-100 text-center">
+              {"I'M ZAKY"}
+            </h1>
+            <h1 className="text-5xl font-bold text-slate-100 text-center">
+              {"FRONTEND WEB DEVELOPER"}
+            </h1>
+            <ButtonLearnMore />
+          </div>
+          <div className="w-[15px] h-[15px] bg-red-400 absolute left-0 top-20" />
+          <div className="w-[1px] h-[100%] bg-slate-300 absolute left-[300px] top-0" />
+        </div>
+      </section>
+      <section className="LATEST-PROJECT w-full">
+        <div className="mx-auto w-[70%] flex flex-col border-l border-black py-10 gap-3">
+          <div className="text-7xl text-red-500 font-extrabold">
+            LATEST PROJECT
+          </div>
+          <div className="grid grid-cols-3 gap-4">
+            <LatestProject />
+            <LatestProject />
+            <LatestProject />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 export default Home;
