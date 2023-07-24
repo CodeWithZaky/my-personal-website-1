fd0dde8ff523a16b060626ba6518899d7b2e95fb
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-07-24 09:57:36 +0700

    update color and gap

--- src/components/MainNavbar.tsx
+++ src/components/MainNavbar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const MainNavbar = () => {
   return (
-    <div className="w-full flex justify-center items-center gap-7 text-xl font-bold font-mono py-7 bg-black text-white border-b border-slate-800">
+    <div className="w-full flex justify-center items-center gap-10 text-xl font-bold font-mono py-7 bg-black text-valorantWhite border-b border-slate-800">
       <Link href={"/"}>HOME</Link>
       <Link href={"/about"}>ABOUT</Link>
       <Link href={"/skill/all"}>SKILL</Link>
