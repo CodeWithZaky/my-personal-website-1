9587aaf6a66dcfb4cfc153d082f9a2a2e182fa3f
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-07-24 10:00:41 +0700

    update about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -6,27 +6,43 @@ export const metadata: Metadata = {
 
 const About = () => {
   return (
-    <div className="w-[70%] m-auto font-mono border flex justify-between items-start pb-10">
-      <div className="w-[70%] border space-y-8">
-        <p className="text-5xl font-bold border">About Me</p>
-        <div className="space-y-4 pr-5">
-          <p>
-            My name is Ahmad Zaky Ubaidillah, but you can call me Zaky or Ubed.
-          </p>
-          <p>
-            For over 2 years developing and programming interfaces with Next js,
-            Tailwind css and Typescript.
-          </p>
-          <p>
-            study of Systems Analysis and Development from Informatics
-            Engineering.
-          </p>
-          <p>
-            Interest in Front-end development with Next js, Tailwind css, and
-            Typescript.
-          </p>
-          <p>Trying to be a little better than yesterday every day.</p>
+    <div className="w-full h-[100vh] bg-valorantWhite flex justify-center items-center relative">
+      <div className="w-[70%] h-full border-l-2 border-black flex justify-start items-start">
+        <div className="w-full flex flex-col justify-center items-start space-y-5 mt-10">
+          <p className="text-8xl font-bold border">ABOUT ME</p>
+          <div className="bg-valorant w-[20px] h-[20px]" />
+          <div className="space-y-2 font-semibold">
+            <p>
+              My name is Ahmad Zaky Ubaidillah, but you can call me Zaky or
+              Ubed.
+            </p>
+            <p>
+              For over 2 years developing and programming interfaces with Next
+              js, Tailwind css and Typescript.
+            </p>
+            <p>
+              study of Systems Analysis and Development from Informatics
+              Engineering.
+            </p>
+            <p>
+              Interest in Front-end development with Next js, Tailwind css, and
+              Typescript.
+            </p>
+            <p>Trying to be a little better than yesterday every day.</p>
+          </div>
+          <div className="flex flex-col justify-center items-start gap-4">
+            <div className="flex gap-5 self-end">
+              <div className="w-[20px] h-[20px] bg-valorant" />
+              <div className="w-[20px] h-[20px] bg-valorant" />
+            </div>
+            <div className="w-[400px] h-[100px] bg-valorant" />
+          </div>
+          <div className="w-[20px] h-[20px] bg-valorant self-end" />
         </div>
+      </div>
+      <div className="absolute top-[3rem] right-0 w-[500px] flex flex-col items-end gap-4">
+        <div className="w-full h-[100px] bg-valorant" />
+        <div className="w-full h-[3px] bg-valorant" />
       </div>
     </div>
   );
