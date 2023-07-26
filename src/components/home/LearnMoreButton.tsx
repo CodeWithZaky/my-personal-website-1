22ed4e8401ce88d5289bd667af47cdca6955b2bb
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-07-26 09:29:41 +0700

    add route link

--- src/components/home/LearnMoreButton.tsx
+++ src/components/home/LearnMoreButton.tsx
@@ -1,9 +1,14 @@
+import Link from "next/link";
+
 const LearnMoreButton = () => {
   return (
     <div className="relative border border-slate-200 p-2 flex justify-center items-center z-1 m-4">
-      <button className="bg-valorant text-2xl text-center font-semibold text-white px-10 py-2 z-10">
+      <Link
+        href={"/about"}
+        className="bg-valorant text-2xl text-center font-semibold text-white px-10 py-2 z-10"
+      >
         Learn more
-      </button>
+      </Link>
       <div className="absolute w-[110%] h-3 bg-black z-2" />
     </div>
   );
