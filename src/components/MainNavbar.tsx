190cc0929af41e87c7a49e1ca13378e52747b118
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-07-26 20:54:53 +0700

    new responsive navbar

--- src/components/MainNavbar.tsx
+++ src/components/MainNavbar.tsx
@@ -1,17 +1,64 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import logoz2 from "@/assets/logoz2.svg";
+import { IoCloseSharp } from "react-icons/io5";
 
 const MainNavbar = () => {
+  const [toggle, setToggle] = useState(false);
+  const toggleButton = () => {
+    setToggle(!toggle);
+  };
+
+  console.log(toggle);
+
   return (
-    <div className="w-full flex justify-center items-center gap-10 text-xl font-bold font-mono py-7 bg-black text-valorantWhite border-b border-white/5">
-      <Image alt="image" src={logoz2} className="h-7 w-auto" />
-      <Link href={"/"}>HOME</Link>
-      <Link href={"/about"}>ABOUT</Link>
-      <Link href={"/skill"}>SKILL</Link>
-      <Link href={"/project"}>PROJECT</Link>
-      <Link href={"/contact"}>CONTACT</Link>
+    <div className="w-full h-[90px]  bg-black border-b border-white/5 flex justify-between md:justify-center items-center gap-10">
+      <Image alt="image" src={logoz2} className="h-7 w-auto ml-10 md:ml-0" />
+      <div className="hidden md:flex justify-center items-center gap-10 text-xl font-bold font-mono  text-valorantWhite">
+        <Link href={"/"}>HOME</Link>
+        <Link href={"/about"}>ABOUT</Link>
+        <Link href={"/skill"}>SKILL</Link>
+        <Link href={"/project"}>PROJECT</Link>
+        <Link href={"/contact"}>CONTACT</Link>
+      </div>
+      <div
+        onClick={toggleButton}
+        className="flex md:hidden flex-col justify-center items-center gap-1 mr-10 md:mr-0"
+      >
+        <div className="w-[30px] h-[5px] rounded-md bg-valorantWhite" />
+        <div className="w-[30px] h-[5px] rounded-md bg-valorantWhite" />
+        <div className="w-[30px] h-[5px] rounded-md bg-valorantWhite" />
+      </div>
+      <section
+        className={`${
+          toggle ? "hidden" : "flex"
+        } w-full h-full absolute top-0 right-0 z-20`}
+      >
+        <div
+          onClick={toggleButton}
+          className="w-[40%] backdrop-blur-md bg-black/30"
+        />
+        <div
+          className={` w-[60%] h-full flex-col justify-start items-center bg-black gap-10 border-l-2  border-l-white/5`}
+        >
+          <div className="w-full h-[90px] border-b border-b-white/5 flex justify-between items-center ">
+            <Image alt="image" src={logoz2} className="h-7 w-auto ml-10" />
+            <IoCloseSharp
+              onClick={toggleButton}
+              className="mr-10 text-3xl text-valorantWhite"
+            />
+          </div>
+          <div className="h-full flex flex-col justify-center items-center gap-20 font-bold font-mono text-xl text-valorantWhite ">
+            <Link href={"/"}>HOME</Link>
+            <Link href={"/about"}>ABOUT</Link>
+            <Link href={"/skill"}>SKILL</Link>
+            <Link href={"/project"}>PROJECT</Link>
+            <Link href={"/contact"}>CONTACT</Link>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
