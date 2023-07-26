2a82078cd718f030e73eaea6897b5ec51f34e94f
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-07-26 14:23:11 +0700

    add new logo

--- src/components/MainNavbar.tsx
+++ src/components/MainNavbar.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import logoZ from "@/assets/logoZ.svg";
+import logoz2 from "@/assets/logoz2.svg";
 
 const MainNavbar = () => {
   return (
     <div className="w-full flex justify-center items-center gap-10 text-xl font-bold font-mono py-7 bg-black text-valorantWhite border-b border-white/5">
-      <Image alt="image" src={logoZ} className="h-7 w-auto" />
+      <Image alt="image" src={logoz2} className="h-7 w-auto" />
       <Link href={"/"}>HOME</Link>
       <Link href={"/about"}>ABOUT</Link>
       <Link href={"/skill"}>SKILL</Link>
