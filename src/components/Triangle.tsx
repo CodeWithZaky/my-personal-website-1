4864be050dc31b74b30a569d51057f7dbf14c47c
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-07-27 08:18:13 +0700

    update responsive

--- src/components/Triangle.tsx
+++ src/components/Triangle.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Triangle = () => {
   return (
     <div className="w-auto overflow-hidden inline-block absolute bottom-0 left-0">
-      <div className="h-[200px] w-[100px] bg-valorantWhite -rotate-45 transform origin-bottom-right"></div>
+      <div className="h-[50px] w-[25px] sm:h-[100px] sm:w-[50px] md:h-[200px] md:w-[100px] bg-valorantWhite -rotate-45 transform origin-bottom-right"></div>
     </div>
   );
 };
