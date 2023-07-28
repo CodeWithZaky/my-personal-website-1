457f5f4b05b19778aa9d706007b7cce4d936c8fd
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-07-28 10:04:22 +0700

    add background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ interface Props {
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-valorantWhite">
         <main className="container-none w-full min-h-screen">
           <MainNavbar />
           {children}
