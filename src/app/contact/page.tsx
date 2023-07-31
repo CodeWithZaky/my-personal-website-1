035443386700d8057f3ad02f6d7507ffba204f58
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2023-07-31 20:30:01 +0700

    update responsive

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -24,17 +24,17 @@ const contacts = [github, instagram, linkedin, gamil, facebook];
 
 const ContactPage = () => {
   return (
-    <div className="w-full min-h-screen flex flex-col justify-start items-center bg-black">
-      <section className="w-full h-[80vh]  flex flex-row ">
+    <div className="w-full min-h-screen flex flex-col justify-center items-center bg-black">
+      <section className="w-full min-h-[80vh]  flex flex-row ">
         <section className="relative w-[15%]">
           <RedSquare possition={"top-5 right-0"} />
           <GreySquare possition={"top-[50%] right-0"} />
         </section>
         <section className="w-[70%] flex flex-col justify-center items-center text-valorantWhite gap-10">
-          <div className="text-3xl md:text-5xl text-center font-semibold">
+          <div className="text-2xl sm:text-3xl md:text-5xl text-center font-semibold">
             CONTACT US
           </div>
-          <div className="flex flex-row flex-wrap justify-center items-center text-4xl md:text-5xl gap-7">
+          <div className="flex flex-row flex-wrap justify-center items-center text-3xl sm:text-4xl md:text-5xl gap-7">
             {contacts.map((contact, index) => {
               return (
                 <div key={index} className="p-5 bg-white/10 rounded-3xl">
@@ -49,7 +49,7 @@ const ContactPage = () => {
           <GreySquare possition={"top-[30%] left-0"} />
         </section>
       </section>
-      <section className="relative w-full h-[20vh] ">
+      <section className="relative w-full h-[20vh]">
         <div className="absolute flex gap-4 right-0 top-0">
           <div className="border border-valorant w-[25px] md:w-[50px] h-[15px]" />
           <div className="bg-valorant w-[100px] md:w-[200px] h-[15px]" />
