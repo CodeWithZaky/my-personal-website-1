02ada1e3dd5153624b7d397b5e742f5357f66e67
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-11 10:05:19 +0700

    reduce border width

--- src/app/skill/page.tsx
+++ src/app/skill/page.tsx
@@ -9,18 +9,18 @@ const skills = ["next js", "tailwind", "typescript", "redux toolkit"];
 
 const Skill = () => {
   return (
-    <div className="h-screen w-full">
+    <div className="w-full h-screen">
       <section className="h-[12%] w-full flex">
-        <div className="w-[20%] h-full bg-valorant border-b-2 border-white" />
-        <div className="w-[80%] h-full bg-valorant border-b-2 border-l-2 border-white" />
+        <div className="w-[20%] h-full bg-valorant border-b border-white" />
+        <div className="w-[80%] h-full bg-valorant border-b border-l border-white" />
       </section>
 
       <section className="h-[63%] w-full flex relative">
         <div className="w-[20%] h-full bg-valorant relative">
           <div className="w-[100px] h-[3px] bg-valorant absolute bottom-[3rem] right-0" />
         </div>
-        <div className="w-[80%] h-full text-valorantWhite bg-valorant border-l-2 border-white space-y-8">
-          <h1 className="text-6xl md:text-8xl font-bold">MY SKILLS</h1>
+        <div className="w-[80%] h-full text-valorantWhite bg-valorant border-l border-white space-y-8">
+          <h1 className="text-6xl font-bold md:text-8xl">MY SKILLS</h1>
           <div className="space-y-3">
             {skills.map((skill, index) => {
               return (
@@ -38,8 +38,8 @@ const Skill = () => {
 
       <section className="h-[15%] w-full flex relative">
         <div className="w-[20%] h-full" />
-        <div className="w-[80%] h-full border-l-2 border-white">
-          <div className="flex flex-row justify-end items-end gap-5 absolute right-0 bottom-5">
+        <div className="w-[80%] h-full border-l border-white">
+          <div className="absolute right-0 flex flex-row items-end justify-end gap-5 bottom-5">
             <div className="w-[30px] h-[10px] border border-valorant" />
             <div className="w-[60px] h-[10px] border border-valorant" />
             <div className="w-[100px] h-[10px] bg-valorant" />
