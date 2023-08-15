c82bf77b431df67ee643bcce40231b130478d05c
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-15 10:19:48 +0700

    add font

---
+++ src/font/font.ts
@@ -0,0 +1,15 @@
+import localFont from "next/font/local";
+
+const TungsenBold = localFont({
+    src: "./font-face/Tungsen-Bold.woff2",
+    display: "swap",
+  });
+
+  
+const DINNextW1G = localFont({
+    src: "./font-face/DINNextW1G.woff2",
+    display: "swap",
+  });
+  
+
+  export {TungsenBold,DINNextW1G}
