1c0a105eaa7a75e6f38b38cb2ecf3f9e8080768b
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-16 06:48:05 +0700

    add contact component and fix css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from "next/types";
 import { TubmnailArray } from "@/src/components/pages/project/TubmnailProject";
 import { DINNextW1G } from "../font/font";
 import clsx from "clsx";
+import Contacts from "../components/pages/contact/Contacts";
 
 export const metadata: Metadata = {
   title: "Home - Portfolio",
@@ -12,10 +13,10 @@ export const metadata: Metadata = {
 const Home = () => {
   return (
     <section>
-      <div className="relative w-full overflow-hidden bg-black">
+      <div className="relative w-full overflow-hidden bg-valorantBlack">
         <TriangleHome />
-        <div className="relative mx-auto w-[70%] flex flex-col border-x border-valorantWhite">
-          <div className="flex flex-col items-center justify-center w-full gap-4 py-16 m-auto tracking-widest">
+        <div className="relative mx-auto w-[70%] flex flex-col border-x border-valorantWhite/50">
+          <div className="flex flex-col items-center justify-center w-full gap-4 py-8 m-auto tracking-widest">
             <div className="w-[15px] h-[15px] bg-valorant self-start" />
             <h3
               className={clsx(
@@ -23,7 +24,7 @@ const Home = () => {
                 "text-xl font-bold text-center md:text-2xl text-valorantWhite tracking-widest"
               )}
             >
-              welcome to my personal wesite
+              welcome to my personal website
             </h3>
             <h1 className="font-bold text-center text-7xl md:text-8xl text-valorantWhite tracking-[.4rem]">
               {"I'M ZAKY"}
@@ -32,13 +33,14 @@ const Home = () => {
               FRONTEND WEB DEVELOPER
             </h1>
             <LearnMoreButton />
+            <div className="w-[15px] h-[15px] border border-valorant self-end mr-3" />
           </div>
-          <div className="w-[1px] h-[100%] bg-valorantWhite absolute left-[300px] top-0" />
+          <div className="w-[1px] h-[100%] bg-valorantWhite/50 absolute left-[300px] top-0" />
         </div>
       </div>
       <div className="w-full bg-valorantWhite">
-        <div className="mx-auto w-[70%] flex flex-col border-l border-black py-10 gap-3">
-          <div className="z-10 -mb-12 -ml-1 text-5xl font-bold md:text-9xl text-valorant">
+        <div className="mx-auto w-[70%] flex flex-col border-l border-gray-500 pt-10 pb-20 gap-3">
+          <div className="z-10 -mb-12 -ml-1 font-bold text-7xl md:text-9xl text-valorant">
             LATEST PROJECT
           </div>
           <div className="grid grid-cols-1 gap-5 md:grid-cols-1 lg:grid-cols-3">
@@ -48,6 +50,7 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <Contacts />
     </section>
   );
 };
