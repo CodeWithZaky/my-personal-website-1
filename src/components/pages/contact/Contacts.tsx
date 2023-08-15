ba2e6b5d4bf6d05ed0688aacd7ebdfb0cf1dd928
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-16 06:52:26 +0700

    create contact page component

---
+++ src/components/pages/contact/Contacts.tsx
@@ -0,0 +1,64 @@
+import {
+  SiGithub,
+  SiInstagram,
+  SiLinkedin,
+  SiGmail,
+  SiFacebook,
+  SiYoutube,
+} from "react-icons/si";
+import Copyright from "@/src/components/pages/contact/Copyright";
+import clsx from "clsx";
+
+const socialIcons: { icon: React.ReactNode; url: string }[] = [
+  {
+    icon: <SiLinkedin />,
+    url: "./",
+  },
+  {
+    icon: <SiInstagram />,
+    url: "./",
+  },
+  {
+    icon: <SiYoutube />,
+    url: "./",
+  },
+  {
+    icon: <SiGmail />,
+    url: "./",
+  },
+  {
+    icon: <SiFacebook />,
+    url: "./",
+  },
+  {
+    icon: <SiGithub />,
+    url: "./",
+  },
+];
+
+const ContactPage = () => {
+  return (
+    <div className="flex flex-col items-end justify-center w-[70%] m-auto border-t border-r border-valorantBlack py-20 gap-5">
+      <div className="text-2xl font-semibold text-center sm:text-3xl md:text-5xl text-valorantBlack">
+        REACH ME OUT
+      </div>
+      <div className="flex flex-row flex-wrap items-center justify-end gap-1 text-2xl sm:text-3xl md:text-4xl text-valorantBlack">
+        {socialIcons.map((contact, index) => {
+          return (
+            <div
+              key={index}
+              className={clsx(
+                "p-3 border border-valorantBlack hover:bg-valorantBlack/5 rounded-xl md:rounded-2xl"
+              )}
+            >
+              {contact.icon}
+            </div>
+          );
+        })}
+      </div>
+      <Copyright />
+    </div>
+  );
+};
+
+export default ContactPage;
