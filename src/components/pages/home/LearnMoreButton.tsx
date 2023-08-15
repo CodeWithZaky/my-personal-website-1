9b940a40d20d3c795b083917549ab26f2b9f24b6
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-16 06:54:13 +0700

    update new css

--- src/components/pages/home/LearnMoreButton.tsx
+++ src/components/pages/home/LearnMoreButton.tsx
@@ -1,18 +1,16 @@
-import { DINNextW1G } from "@/src/font/font";
-import clsx from "clsx";
 import Link from "next/link";
 
 const LearnMoreButton = () => {
   return (
-    <div className="relative flex flex-col items-center justify-center gap-3 p-3 m-4 z-1">
-      <div className="absolute top-0 w-full border-t border-l border-r h-[40%]"></div>
+    <div className="relative flex flex-col items-center justify-center p-2 m-4 z-1">
+      <div className="absolute top-0 w-full border-t border-l border-r h-[40%] border-valorantWhite"></div>
       <Link
         href={"/about"}
-        className="z-10 px-2 text-2xl font-semibold text-center text-white bg-valorant md:px-10 md:py-2"
+        className="z-10 px-2 text-2xl font-semibold text-center text-valorantWhite bg-valorant md:px-10 md:py-2"
       >
         MORE ABOUT ME
       </Link>
-      <div className="absolute bottom-0 w-full border-b border-l border-r h-[40%]"></div>
+      <div className="absolute bottom-0 w-full border-b border-l border-r h-[40%] border-valorantWhite"></div>
     </div>
   );
 };
