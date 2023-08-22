0f8d26b2815c0fe36a9b8c741674508ec59c1b09
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-22 22:13:56 +0700

    new split file LatesProject

---
+++ src/components/pages/home/LatestProject.tsx
@@ -0,0 +1,20 @@
+import { TubmnailArray } from "@/components/pages/projects/TubmnailProject";
+
+const LatestProject = () => {
+  return (
+    <div className="w-full">
+      <div className="group mx-auto w-[70%] flex flex-col border-l border-valorantBlack/50 pt-10 pb-20 gap-3">
+        <div className="z-10 -mb-12 -ml-1 font-bold transition-all group-hover:-mb-5 text-7xl md:text-9xl text-valorant">
+          LATEST PROJECT
+        </div>
+        <div className="grid grid-cols-1 gap-5 transition-all md:grid-cols-1 lg:grid-cols-3">
+          {TubmnailArray.slice(0, 3).map((item, index) => {
+            return <div key={index.toString()}>{item}</div>;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LatestProject;
