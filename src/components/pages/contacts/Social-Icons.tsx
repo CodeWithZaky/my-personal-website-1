f0379c7aa04022f5fcf38debc1da8000d2c01a0d
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-22 22:14:29 +0700

    update URL Linkedin

--- src/components/pages/contacts/Social-Icons.tsx
+++ src/components/pages/contacts/Social-Icons.tsx
@@ -3,7 +3,7 @@ import { SiGithub, SiInstagram, SiLinkedin } from "react-icons/si";
 export const socialIcons: { icon: React.ReactNode; url: string }[] = [
   {
     icon: <SiLinkedin />,
-    url: "https://www.linkedin.com/in/ahmad-zaky-ubaidillah-1b2721276",
+    url: "https://www.linkedin.com/in/ahmad-zaky-ubaidillah",
   },
   {
     icon: <SiInstagram />,
