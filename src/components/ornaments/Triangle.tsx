a854e75ef896984ca278a5b7b9ea7c2701e2a3e1
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-08-22 22:17:16 +0700

    remove unused items

--- src/components/ornaments/Triangle.tsx
+++ src/components/ornaments/Triangle.tsx
@@ -1,14 +1,7 @@
 export function TriangleHome() {
   return (
-    <div className="w-auto overflow-hidden inline-block absolute bottom-0 left-0">
+    <div className="absolute bottom-0 left-0 inline-block w-auto overflow-hidden">
       <div className="h-[50px] w-[25px] sm:h-[100px] sm:w-[50px] md:h-[200px] md:w-[100px] bg-valorantWhite -rotate-45 transform origin-bottom-right"></div>
-    </div>
-  );
-}
-export function TriangleSkill() {
-  return (
-    <div className="w-auto overflow-hidden inline-block absolute bottom-0 right-0">
-      <div className="h-[200px] w-[100px] md:h-[500px] md:w-[270px] bg-valorantWhite rotate-45 transform origin-bottom-left"></div>
     </div>
   );
 }
