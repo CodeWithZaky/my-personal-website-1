4247c40d4ee5627d6dea2f44e1c24d62bfe23ca3
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-01 10:14:26 +0700

    refactoring projects page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next/types";
-import { thumbnail_projects } from "@/src/components/atoms/items/Tubmnail-Projects";
-import clsx from "clsx";
+import ProjectContent from "@/components/organisms/projects/ProjectContent";
 
 export const metadata: Metadata = {
   title: "Project - Portfolio",
@@ -9,46 +8,25 @@ export const metadata: Metadata = {
 
 const ProjectsPage = () => {
   return (
-    <div className="w-full min-h-screen">
-      <div className="h-[4rem] w-full flex">
+    <section className="w-full min-h-screen">
+      <SectionGrid className="h-[4rem] w-full flex">
         <div className="w-full md:w-[80%] border-r border-valorantBlack/50" />
         <div className="w-0 md:w-[20%] " />
-      </div>
+      </SectionGrid>
       <div className="flex w-full">
-        <div className="w-full md:w-[80%] border-r border-t border-r-valorantBlack/50 border-t-valorantBlack/50 flex flex-col justify-end items-end">
-          <div className="w-[10px] h-[10px] bg-valorant" />
-          <h1 className="z-10 font-bold text-7xl text-valorant sm:-mb-0 md:-mb-9 md:text-8xl lg:text-9xl">
-            PROJECTS
-          </h1>
-          <div className="w-[90%] md:w-[80%] grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 border-l border-valorantBlack/50 gap-3">
-            {thumbnail_projects.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className={clsx(
-                    // index == 0
-                    //   ? "border-r border-b"
-                    //   : index == 1
-                    //   ? "border-l border-b"
-                    //   :
-                    index % 2 == 1
-                      ? "lg:border-l border-t border-b"
-                      : index % 2 == 0
-                      ? "lg:border-r border-t border-b"
-                      : "",
-                    "p-5 border-valorantBlack/50"
-                  )}
-                >
-                  {item}
-                </div>
-              );
-            })}
-          </div>
-        </div>
-        <div className="w-0 md:w-[20%] border-t border-t-valorantBlack/50" />
+        <ProjectContent />
+        <SectionGrid className="w-0 md:w-[20%] border-t border-t-valorantBlack/50" />
       </div>
-    </div>
+    </section>
   );
 };
 
 export default ProjectsPage;
+
+const SectionGrid = ({
+  children,
+  className,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) => <div className={className}>{children}</div>;
--- src/components/molecules/CardProject.tsx
+++ src/components/molecules/CardProject.tsx
@@ -2,16 +2,16 @@ import { DINNextW1G } from "@/src/assets/fonts/font";
 import clsx from "clsx";
 import Image, { StaticImageData } from "next/image";
 
-interface PropsType {
+interface CardProjectType {
   srcTumb: StaticImageData;
   title: string;
-  stackTumb: string[];
-  textTumb: string;
+  stack: string[];
+  description: string;
   linkHref: string;
 }
 
-const CardProject = (props: PropsType) => {
-  const { linkHref, title, srcTumb, stackTumb, textTumb } = props;
+const CardProject = (props: CardProjectType) => {
+  const { linkHref, title, srcTumb, stack, description } = props;
   return (
     <a
       href={`${linkHref}`}
@@ -21,39 +21,26 @@ const CardProject = (props: PropsType) => {
         "flex flex-col items-start justify-start w-full gap-2 cursor-pointer"
       )}
     >
-      <ImageProject alt={title} src={srcTumb} />
-      <Title title={title} />
-      <Stack stackTumb={stackTumb} />
-      <Description text={textTumb} />
+      <Image
+        alt={title}
+        src={srcTumb}
+        className="w-full h-full mb-2 border border-valorantBlack/10"
+      />
+      <h1 className="text-xl font-bold text-valorantBlack hover:underline">
+        {title}
+      </h1>
+      <div className="flex flex-wrap items-start justify-start gap-1 text-xs text-valorantWhite">
+        {stack.map((item, index) => (
+          <p key={index} className="px-2 bg-valorantBlack">
+            {item}
+          </p>
+        ))}
+      </div>
+      <p className="font-bold leading-none text-md md:text-md text-valorantBlack/80 hover:text-valorantBlack">
+        {description}
+      </p>
     </a>
   );
 };
 
 export default CardProject;
-
-const ImageProject = ({ alt, src }: { alt: string; src: StaticImageData }) => (
-  <Image
-    alt={alt}
-    src={src}
-    className="w-full h-full mb-2 border border-valorantBlack/10"
-  />
-);
-const Title = ({ title }: { title: string }) => (
-  <h1 className="text-xl font-bold text-valorantBlack hover:underline">
-    {title}
-  </h1>
-);
-const Stack = ({ stackTumb }: { stackTumb: string[] }) => (
-  <div className="flex flex-wrap items-start justify-start gap-1 text-xs text-valorantWhite">
-    {stackTumb.map((item, index) => (
-      <p key={index} className="px-2 bg-valorantBlack">
-        {item}
-      </p>
-    ))}
-  </div>
-);
-const Description = ({ text }: { text: string }) => (
-  <p className="font-bold leading-none text-md md:text-md text-valorantBlack/80 hover:text-valorantBlack">
-    {text}
-  </p>
-);
