c378b5a1014e3d10d99929b92b86672be26a5290
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-01 10:15:58 +0700

    initialize context

--- src/components/organisms/nav/Navbar.tsx
+++ src/components/organisms/nav/Navbar.tsx
@@ -5,27 +5,21 @@ import newLogoZ from "@/assets/images/newLogoZ.svg";
 import Hamburger from "../../atoms/buttons/Hamburger";
 import NavLink from "../../molecules/NavLink";
 
-interface Props {
-  toggleButton: () => void;
-}
-
-const MainNavbar = ({ toggleButton }: Props) => {
+const MainNavbar = () => {
   return (
-    <>
-      <section className="z-20 flex items-center justify-between w-full gap-10 border-b bg-valorantBlack py-7 border-valorantWhite/10 md:justify-center">
-        <Link href={"/"}>
-          <Image
-            alt="image"
-            src={newLogoZ}
-            className="w-auto h-6 ml-10 md:ml-0"
-          />
-        </Link>
-        <div className="items-center justify-center hidden gap-10 font-mono text-xl font-bold md:flex text-valorantWhite">
-          <NavLink />
-        </div>
-        <Hamburger toggleButton={toggleButton} />
-      </section>
-    </>
+    <section className="z-20 flex items-center justify-between w-full gap-10 border-b bg-valorantBlack py-7 border-valorantWhite/10 md:justify-center">
+      <Link href={"/"}>
+        <Image
+          alt="image"
+          src={newLogoZ}
+          className="w-auto h-6 ml-10 md:ml-0"
+        />
+      </Link>
+      <div className="items-center justify-center hidden gap-10 font-mono text-xl font-bold md:flex text-valorantWhite">
+        <NavLink />
+      </div>
+      <Hamburger />
+    </section>
   );
 };
 
--- src/components/organisms/nav/Sidebar.tsx
+++ src/components/organisms/nav/Sidebar.tsx
@@ -1,47 +1,55 @@
 import newLogoZ from "@/assets/images/newLogoZ.svg";
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import { IoCloseSharp } from "react-icons/io5";
 import NavLink from "../../molecules/NavLink";
+import { useContext } from "react";
+import { ToggleContext } from "@/src/context/ToggleButton";
 
-interface Props {
-  toggleButton: () => void;
-  toggle: boolean;
-}
+export default function Sidebar() {
+  const { isToggled } = useContext(ToggleContext);
 
-const Sidebar = ({ toggleButton, toggle }: Props) => {
   return (
     <section
       className={
-        toggle
+        isToggled
           ? "flex w-full h-full absolute top-0 right-0 z-20 md:hidden"
           : "hidden"
       }
     >
-      <div
-        onClick={toggleButton}
-        className="w-[40%] backdrop-blur-sm bg-black/30"
-      />
-      <div
-        className={
-          "w-[60%] h-full flex-col justify-start items-center bg-black gap-10 border-l-2  border-l-white/5"
-        }
-      >
-        <div className="w-full h-[90px] border-b border-b-white/5 flex justify-between items-center ">
-          <Link href={"/"}>
-            <Image alt="image" src={newLogoZ} className="w-auto h-6 ml-10" />
-          </Link>
-          <IoCloseSharp
-            onClick={toggleButton}
-            className="mr-10 text-4xl text-valorantWhite"
-          />
-        </div>
-        <div className="flex flex-col items-end justify-center gap-10 pt-10 pr-10 font-mono text-xl font-bold text-valorantWhite">
-          <NavLink />
-        </div>
+      <Outside />
+      <div className="w-[60%] h-full flex-col justify-start items-center bg-black gap-10 border-l-2  border-l-white/5">
+        <HeaderSidebar newLogoZ={newLogoZ} />
+        <BodySidebar />
       </div>
     </section>
   );
+}
+
+const Outside = () => {
+  const { toggleFn } = useContext(ToggleContext);
+  return (
+    <div onClick={toggleFn} className="w-[40%] backdrop-blur-sm bg-black/30" />
+  );
 };
 
-export default Sidebar;
+const HeaderSidebar = ({ newLogoZ }: { newLogoZ: StaticImageData }) => {
+  const { toggleFn } = useContext(ToggleContext);
+  return (
+    <div className="w-full h-[90px] border-b border-b-white/5 flex justify-between items-center ">
+      <Link href={"/"}>
+        <Image alt="image" src={newLogoZ} className="w-auto h-6 ml-10" />
+      </Link>
+      <IoCloseSharp
+        onClick={toggleFn}
+        className="mr-10 text-4xl text-valorantWhite"
+      />
+    </div>
+  );
+};
+
+const BodySidebar = () => (
+  <div className="flex flex-col items-end justify-center gap-10 pt-10 pr-10 font-mono text-xl font-bold text-valorantWhite">
+    <NavLink />
+  </div>
+);
