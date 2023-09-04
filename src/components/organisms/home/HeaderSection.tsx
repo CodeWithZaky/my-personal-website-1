eb907f23e09d5e91f47fed8da8256f1e2621c33f
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-04 21:16:40 +0700

    add new style css

--- src/components/organisms/home/HeaderSection.tsx
+++ src/components/organisms/home/HeaderSection.tsx
@@ -10,10 +10,10 @@ const HeaderSection = () => {
       <Triangle />
       <div className="relative mx-auto w-[70%] flex flex-col border-x border-valorantWhite/50">
         <div className="flex flex-col items-center justify-center w-full gap-4 py-8 m-auto tracking-widest">
-          <Square className="w-[15px] h-[15px] bg-valorant self-start" />
+          <Square className="w-[15px] h-[15px] bg-valorant hover:bg-transparent hover:border hover:border-valorant self-start transition-all" />
           <SelfIntroductios />
           <LearnMoreButton />
-          <Square className="w-[15px] h-[15px] border border-valorant self-end mr-3" />
+          <Square className="w-[15px] h-[15px] border border-valorant hover:border-none hover:bg-valorant self-end mr-3 transition-all" />
         </div>
         <VerticalLine className="w-[1px] h-[100%] bg-valorantWhite/50 absolute left-[300px] top-0" />
       </div>
