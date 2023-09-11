7a792ec1eb7cc6e2d8e7e569c3c0ce9ee015f102
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-11 09:08:30 +0700

    add skill items

--- src/components/atoms/data/Skilll-Items.tsx
+++ src/components/atoms/data/Skilll-Items.tsx
@@ -3,4 +3,11 @@ export const skill_items: string[] = [
   "next js",
   "tailwind",
   "typescript",
+  "react query",
+  "react hook form",
+  "framer motion",
+  "clsx",
+  "swipper js",
+  "daisy ui",
+  "sweetalert2",
 ];
