f0d7c32e0fb0fc6e023a97b90da47948c2ba1904
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2023-09-12 21:20:51 +0700

    update layout project page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,9 @@
+"use client";
 import { Metadata } from "next/types";
-import ProjectContent from "@/components/organisms/projects/ProjectContent";
+import { useState } from "react";
+import Square from "@/src/components/atoms/ornaments/Square";
+import { thumbnail_projects } from "@/src/components/atoms/data/Tubmnail-Projects";
+import clsx from "clsx";
 
 export const metadata: Metadata = {
   title: "Project - Portfolio",
@@ -7,15 +11,28 @@ export const metadata: Metadata = {
 };
 
 const ProjectsPage = () => {
+  const [widthRightSection] = useState("w-5/6 md:w-4/5");
+  const [widthLeftSection] = useState("w-1/6 md:w-1/5");
+
   return (
     <section className="w-full min-h-screen">
-      <SectionGrid className="h-[4rem] w-full flex">
-        <div className="w-full md:w-[80%] border-r border-valorantBlack/50" />
-        <div className="w-0 md:w-[20%] " />
-      </SectionGrid>
+      <div className="h-[4rem] w-full flex">
+        <div
+          className={clsx(
+            widthRightSection,
+            "border-r border-valorantBlack/50"
+          )}
+        />
+        <div className={widthLeftSection} />
+      </div>
       <div className="flex w-full">
-        <ProjectContent />
-        <SectionGrid className="w-0 md:w-[20%] border-t border-t-valorantBlack/50" />
+        <ProjectContent widthRightSection={widthRightSection} />
+        <div
+          className={clsx(
+            widthLeftSection,
+            "border-t border-t-valorantBlack/50"
+          )}
+        />
       </div>
     </section>
   );
@@ -23,10 +40,46 @@ const ProjectsPage = () => {
 
 export default ProjectsPage;
 
-const SectionGrid = ({
-  children,
-  className,
+const ProjectContent = ({
+  widthRightSection,
 }: {
-  children?: React.ReactNode;
-  className?: string;
-}) => <div className={className}>{children}</div>;
+  widthRightSection: string;
+}) => {
+  return (
+    <div
+      className={clsx(
+        widthRightSection,
+        "flex flex-col items-end justify-end border-t border-r border-r-valorantBlack/50 border-t-valorantBlack/50"
+      )}
+    >
+      <Square className="w-[10px] h-[10px] bg-valorant" />
+      <h1 className="z-10 text-7xl text-valorant sm:-mb-0 md:-mb-9 md:text-8xl lg:text-9xl">
+        PROJECTS
+      </h1>
+      <div className="w-[90%] md:w-[80%] grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 border-l border-valorantBlack/50 gap-3">
+        {thumbnail_projects.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className={clsx(
+                // index == 0
+                //   ? "border-r border-b"
+                //   : index == 1
+                //   ? "border-l border-b"
+                //   :
+                index % 2 == 1
+                  ? "md:border-l border-t border-b"
+                  : index % 2 == 0
+                  ? "md:border-r border-t border-b"
+                  : "",
+                "p-5 border-valorantBlack/50"
+              )}
+            >
+              {item}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
