96d2f9714005598dc45fb4a033dc0ba5ce89355d
Ahmad Zaky Ubaidillah <111162218+zakyscripters@users.noreply.github.com>
2023-11-21 07:58:54 +0700

    rename file

--- src/components/atoms/buttons/MoreAboutMe.tsx
+++ src/components/atoms/buttons/MoreAboutMe.tsx
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-const LearnMoreButton = () => {
+const MoreAboutMe = () => {
   return (
     <div className="relative flex flex-col items-center justify-center p-2 m-4 z-1">
       <div className="absolute top-0 w-full border-t border-l border-r h-[40%] border-valorantWhite"></div>
       <Link
         href={"/about"}
-        className="z-10 px-2 text-2xl text-center transition-all text-valorantWhite bg-valorant hover:bg-valorantBlue md:px-10 md:py-2"
+        className="z-10 py-1 px-5 text-xl md:text-2xl text-center transition-all text-valorantWhite bg-valorant hover:bg-valorantBlue md:px-10 md:py-2"
       >
         MORE ABOUT ME
       </Link>
@@ -14,4 +14,5 @@ const LearnMoreButton = () => {
     </div>
   );
 };
-export default LearnMoreButton;
+
+export default MoreAboutMe;
