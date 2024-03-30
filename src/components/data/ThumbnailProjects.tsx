c99a26181f4b490303bac0289e45f262f13150a4
Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
2024-03-31 06:50:40 +0700

    dev : refactor variable name and change content (#36)
    * reafactor and change link of matrixcalculator on CardProject component
    
    * Revert "reafactor and change link of matrixcalculator on CardProject component"
    
    This reverts commit 5674019c7f135c1b94bee81bded230a9e33543dd.
    
    * change title matrix-calculator at CardProject in ThumbnailProjects
    
    * refactor variable name and change content

--- src/components/data/ThumbnailProjects.tsx
+++ src/components/data/ThumbnailProjects.tsx
@@ -1,23 +1,23 @@
-import matrixcalc from "@/assets/images/tumbnail-project/matrix-calculator-2.png";
-import numbconv from "@/assets/images/tumbnail-project/number-convertion-2.png";
-import myportfolio from "@/assets/images/tumbnail-project/my-portfolio-2.png";
-import gedongarum from "@/assets/images/tumbnail-project/gedongarum.png";
+import matrixCalculatorImg from "@/assets/images/tumbnail-project/matrix-calculator-2.png";
+import numberConverterImg from "@/assets/images/tumbnail-project/number-convertion-2.png";
+import myPortfolioImg from "@/assets/images/tumbnail-project/my-portfolio-2.png";
+import gedongarumImg from "@/assets/images/tumbnail-project/gedongarum.png";
 import CardProject from "../CardProject";
 
-const matrixCalc = (
+const matrixCalcucalor = (
     <CardProject
         linkHref="https://matrixcalculator.vercel.app"
-        srcTumb={matrixcalc}
+        srcTumb={matrixCalculatorImg}
         title="matrix-calculator"
         stack={["NEXT JS", "TAILWIND CSS", "DAISYUI", "SWEETALERT2"]}
         description="This website was built as part of a college assignment to create an application for the addition, subtraction, and multiplication of two matrices"
     />
 );
-const numbConv = (
+const numberConverter = (
     <CardProject
-        linkHref="https://numconv.vercel.app"
-        srcTumb={numbconv}
-        title="numconv"
+        linkHref="number-converter-byzaky.vercel.app"
+        srcTumb={numberConverterImg}
+        title="number-converter"
         stack={["NEXT JS", "TAILWIND CSS", "DAISYUI", "SWEETALERT2"]}
         description="This website was developed as part of a college assignment to create an application for converting decimal, binary, octal, and hexadecimal numbers."
     />
@@ -25,16 +25,16 @@ const numbConv = (
 const myPortfolio = (
     <CardProject
         linkHref="https://zaky.vercel.app"
-        srcTumb={myportfolio}
+        srcTumb={myPortfolioImg}
         title="zaky"
         stack={["NEXT JS", "TAILWIND CSS", "FRAMERMOTION"]}
         description="PERSONAL WEBSITE FOR INFORMATION ABOUT ME, ESPECIALLY IN THE FIELD OF TECHNOLOGY"
     />
 );
-const infogedongarum = (
+const infoGedongarum = (
     <CardProject
         linkHref="https://gedongarum.vercel.app"
-        srcTumb={gedongarum}
+        srcTumb={gedongarumImg}
         title="gedongarum"
         stack={["NEXT JS", "TAILWIND CSS", "SHADCN UI"]}
         description="This website was constructed as part of a college assignment to create an informational website about the hometown village of the student"
@@ -42,8 +42,8 @@ const infogedongarum = (
 );
 
 export const thumbnail_projects = [
-    infogedongarum,
-    matrixCalc,
-    numbConv,
+    infoGedongarum,
+    matrixCalcucalor,
+    numberConverter,
     myPortfolio,
 ];
